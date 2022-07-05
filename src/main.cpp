90670010127587f07a7f52b9dd1d1d29754c6d41
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2022-07-05 11:31:15 -0400

    Transfer google::protobuf::Map to std::map

--- src/main.cpp
+++ src/main.cpp
@@ -38,5 +38,16 @@ int main(int argc, char *argv[]) {
     searchOffset_protobuf(
             temp_map["_ZN5amrex13launch_globalILi256EZNS_11ParallelForIiZNS_7BaseFabIiE6setValILNS_5RunOnE1EEEvRKiRKNS_3BoxENS_8DestCompENS_8NumCompsEEUliiiiE_vEENSt9enable_ifIXsr5amrex19MaybeDeviceRunnableIT0_vEE5valueEvE4typeERKNS_3Gpu10KernelInfoESA_T_OSF_EUlvE_EEvSF_"],
             240);
+    
+    
+    //  Transfer google::protobuf::Map to std::map
+//     std::map<string, kernel::mapRes_FuncInfo> std_map(temp_map.begin(), temp_map.end());
+//     // castro1.txt
+//     searchOffset_protobuf(std_map["_ZN3cub11EmptyKernelIvEEvv"], 0);
+//     searchOffset_protobuf(std_map["_ZN5amrex13launch_globalILi256EZNS_11ParallelForIiZNS_7BaseFabIiE6setValILNS_5RunOnE1EEEvRKiRKNS_3BoxENS_8DestCompENS_8NumCompsEEUliiiiE_vEENSt9enable_ifIXsr5amrex19MaybeDeviceRunnableIT0_vEE5valueEvE4typeERKNS_3Gpu10KernelInfoESA_T_OSF_EUlvE_EEvSF_"], 64);
+//     searchOffset_protobuf(std_map["_ZN5amrex13launch_globalILi256EZNS_11ParallelForIiZNS_7BaseFabIiE6setValILNS_5RunOnE1EEEvRKiRKNS_3BoxENS_8DestCompENS_8NumCompsEEUliiiiE_vEENSt9enable_ifIXsr5amrex19MaybeDeviceRunnableIT0_vEE5valueEvE4typeERKNS_3Gpu10KernelInfoESA_T_OSF_EUlvE_EEvSF_"], 144);
+//     searchOffset_protobuf(std_map["_ZN5amrex13launch_globalILi256EZNS_11ParallelForIiZNS_7BaseFabIiE6setValILNS_5RunOnE1EEEvRKiRKNS_3BoxENS_8DestCompENS_8NumCompsEEUliiiiE_vEENSt9enable_ifIXsr5amrex19MaybeDeviceRunnableIT0_vEE5valueEvE4typeERKNS_3Gpu10KernelInfoESA_T_OSF_EUlvE_EEvSF_"], 240);
+
+    
     return 0;
 }
